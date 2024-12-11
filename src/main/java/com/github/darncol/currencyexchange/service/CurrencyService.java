1ed0c47753c468a81e5ba2f3d3789ae3502ccfbd
Darncol <60666804+Darncol@users.noreply.github.com>
2024-12-11 14:21:44 +0200

    Add AddCurrency controller and enhance CurrencyService
    Introduce the AddCurrency controller to handle currency-related HTTP requests. Enhance CurrencyService by adding a constructor for dependency injection and a method to check currency existence using CurrencyDAO.

--- src/main/java/com/github/darncol/currencyexchange/service/CurrencyService.java
+++ src/main/java/com/github/darncol/currencyexchange/service/CurrencyService.java
@@ -1,5 +1,6 @@
 package com.github.darncol.currencyexchange.service;
 
+import com.github.darncol.currencyexchange.dao.CurrencyDAO;
 import com.github.darncol.currencyexchange.entity.Currency;
 import com.github.darncol.currencyexchange.entity.CurrencyDTO;
 
@@ -7,11 +8,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CurrencyService {
+    private CurrencyDAO currencyDAO;
+
+    public CurrencyService(CurrencyDAO currencyDAO) {
+        this.currencyDAO = currencyDAO;
+    }
+
     public CurrencyDTO currencyToDTO(Currency currency) {
         return new CurrencyDTO(currency);
     }
 
     public List<CurrencyDTO> currencyToDTOs(List<Currency> currencies) {
         return currencies.stream().map(this::currencyToDTO).collect(Collectors.toList());
+    }
+
+    public Boolean isCurrencyExists(String code) {
+        return currencyDAO.getCurrencyByCode(code) != null;
     }
 }
